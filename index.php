7affe78d41585a61f1e3670eeeaf0b7adc053e26
Marcel Hauri <hello@m83.ch>
2010-09-16 14:49:29 +0200

    add index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * txt2img
+ * Copyright (C) 2010, Marcel Hauri
+ * http://blog.m83.ch/ - hello@m83.ch
+ * 
+ * Licensed under the new BSD License
+ * See the License file for details
+ * 
+ * Version: 0.9.0 
+ */
+?>
